8abcba65401d19c1193ed6e0a24a208169587c96
Lombok888 <twsbrearley@gmail.com>
2023-01-26 22:31:13 +0000

    change to scss

--- CompletelyDotty/src/app/app.component.ts
+++ CompletelyDotty/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CompletelyDotty';
+  title = 'Completely Dotty';
+
+  dotsFired: boolean | undefined;
+  bounceDirection: string | undefined;
+
+  fireDotsFired() {
+    this.dotsFired = true;
+  }
 }
--- CompletelyDotty/src/app/app.module.ts
+++ CompletelyDotty/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShareButtonsModule } from 'ngx-sharebuttons/buttons';
+import { MainPaneComponent } from './main-pane/main-pane.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPaneComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ShareButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
